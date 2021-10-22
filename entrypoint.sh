544ce2d6515532dfa985b952d31de8d5fd9b7a20
Rehan Mahmood <rehanone@gmail.com>
2021-10-21 20:55:31 -0400

    Add support for docker based secrets for database password

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-python3 generate_config.py --certbot && certbot -n certonly; crond && ./generate_config.py --postfix && postfix start-fg
+# This function reads the docker secrets based variables defined with pattern *_FILE into the normal variables
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'DB_PASSWORD' 'default_password'
+# (will allow for "$DB_PASSWORD_FILE" to fill in the value of
+#  "$DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+  local var="$1"
+  local fileVar="${var}_FILE"
+  local def="${2:-}"
+  if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+  	echo "Both $var and $fileVar are set (but are exclusive)"
+  fi
+  local val="$def"
+  if [ "${!var:-}" ]; then
+  	val="${!var}"
+  elif [ "${!fileVar:-}" ]; then
+  	val="$(< "${!fileVar}")"
+  fi
+  export "$var"="$val"
+  unset "$fileVar"
+}
 
-# Idea taken from https://github.com/Mailu/Mailu/blob/master/core/postfix/Dockerfile
-HEALTHCHECK --start-period=350s CMD echo QUIT|nc localhost 25|grep "220 .* ESMTP Postfix"
+_main() {
+  # Each environment variable that supports the *_FILE pattern eeds to be passed into the file_env() function.
+  file_env "DB_PASSWORD"
+
+  python3 generate_config.py --certbot && certbot -n certonly; crond && ./generate_config.py --postfix && postfix start-fg
+
+  # Idea taken from https://github.com/Mailu/Mailu/blob/master/core/postfix/Dockerfile
+  HEALTHCHECK --start-period=350s CMD echo QUIT|nc localhost 25|grep "220 .* ESMTP Postfix"
+}
+
+_main "$@"
